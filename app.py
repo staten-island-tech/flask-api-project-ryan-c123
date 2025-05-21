c4329fd0d265257c2f0f56bd8b1f32286ca6391d
ryan-c123 <ryanc545@nycstudents.net>
2025-05-21 10:16:08 -0400

    wowowowowowooww owo

--- app.py
+++ app.py
@@ -9,29 +9,61 @@ can be interpreted many different ways).
 the user sees on the page.
 -You may use more than one API to accomplish the project.
  """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
+app = Flask(__name__)
 
-mortys = []
-base_url = "https://rickandmortyapi.com/api/character/"
-page = 1
+@app.route("/")
+def index():
+    # Step 1: Read page number from query string (default = 1)
+    page = request.args.get("page", default=1, type=int)
 
-while True:
-    response = requests.get(f"{base_url}?page={page}")
+    # Step 2: Get data from API
+    response = requests.get("https://rickandmortyapi.com/api/character", params={"page": page})
     data = response.json()
-    
-    # Filter characters with "Rick" in the name
-    for character in data["results"]:
-        if "morty" in character["name"].lower():
-            mortys.append(character)
-    
-    # Go to the next page if available
-    if data["info"]["next"]:
-        page += 1
-    else:
-        break
+    characters = data["results"]
+    total_pages = data["info"]["pages"]
 
-# Print names of all "Rick" characters
-for morty in mortys:
-    print(morty["name"])
+    # Step 3: Prepare character data
+    char_list = []
+    for char in characters:
+        char_list.append({
+            "id": char["id"],
+            "name": char["name"],
+            "image": char["image"],
+            "species": char["species"],
+            "status": char["status"]
+        })
+
+    # Step 4: Handle next and previous page numbers
+    next_page = page + 1 if data["info"]["next"] else None
+    prev_page = page - 1 if data["info"]["prev"] else None
+
+    # Step 5: Pass everything to template
+    return render_template("index.html", characters=char_list,
+                           next_page=next_page,
+                           prev_page=prev_page,
+                           current_page=page,
+                           total_pages=total_pages)
+@app.route("/character/<int:id>")
+def character_detail(id):
+    response = requests.get(f"https://rickandmortyapi.com/api/character/{id}")
+    data = response.json()
+
+    character = {
+        "id": data["id"],
+        "name": data["name"],
+        "status": data["status"],
+        "species": data["species"],
+        "type": data["type"] if data["type"] else "Unknown",
+        "gender": data["gender"],
+        "origin": data["origin"]["name"],
+        "location": data["location"]["name"],
+        "image": data["image"]
+    }
+
+    return render_template("character.html", character=character)
+
+if __name__ == '__main__':
+    app.run(debug=True)
