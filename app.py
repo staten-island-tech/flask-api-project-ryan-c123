a3e608e0881e85feee09d1ec23256b4920951e52
ryan-c123 <ryanc545@nycstudents.net>
2025-05-30 11:11:12 -0400

    can i get a kss
    and can u make it last forever

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") #this is home page
 def index():
     page = request.args.get("page", default=1, type=int)
     search = request.args.get("search", default="", type=str)
@@ -33,16 +33,16 @@ def index():
         params["gender"] = gender
 
     response = requests.get("https://rickandmortyapi.com/api/character", params=params)
-
+#if the the characters cannot be found js say not found
     if response.status_code == 404:
         return render_template("index.html",
-                               characters=[],
-                               current_page=page,
-                               total_pages=0,
-                               next_page=None,
-                               prev_page=None,
-                               error="No characters found.",
-                               request=request)
+            characters=[],
+            current_page=page,
+            total_pages=0,
+            next_page=None,
+            prev_page=None,
+            error="No characters found.",
+            request=request)
 
     data = response.json()
     characters = data["results"]
@@ -54,23 +54,22 @@ def index():
         "image": char["image"],
         "species": char["species"],
         "status": char["status"]
-    } for char in characters]
-
+    } for char in characters] #page change
     next_page = page + 1 if data["info"]["next"] else None
     prev_page = page - 1 if data["info"]["prev"] else None
-
     return render_template("index.html",
-                           characters=char_list,
-                           current_page=page,
-                           total_pages=total_pages,
-                           next_page=next_page,
-                           prev_page=prev_page,
-                           error=None,
-                           request=request)
+        characters=char_list,
+        current_page=page,
+        total_pages=total_pages,
+        next_page=next_page,
+        prev_page=prev_page,
+        error=None,
+        request=request)
+#app route to display characters individually when u click on their card
 @app.route("/character/<int:character_id>")
 def character_detail(character_id):
     response = requests.get(f"https://rickandmortyapi.com/api/character/{character_id}")
-
+# if the status isn't 200, or if it doesn't work, then say character not found
     if response.status_code != 200:
         return render_template("character.html", character=None, error="Character not found.")
 
